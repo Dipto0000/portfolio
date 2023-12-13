75868dec4c7a384b74ff64d18c5b17104564e6d4
Shahriar Ahmed <dipto2041@gmail.com>
2023-12-13 23:27:25 +0600

    favicon added & title changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero';
+import HomeDescreption from './HomeDescreption';
 
 const Home = () => {
     return (
-        <div>
+        <div className=''>
             <Hero></Hero>
+            <HomeDescreption></HomeDescreption>
         </div>
     );
 };
--- src/pages/HomeDescreption.js
+++ src/pages/HomeDescreption.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from "react";
 
 const HomeDescreption = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="w-1/2">
+      <p className="">
+        Hello there! I am currently an undergraduate student pursuing a degree
+        in Industrial and Production Engineering at{" "}
+        <a
+          className="text-blue-500"
+          href="https://www.sust.edu/d/ipe/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Shahjalal University of Science & Technology
+        </a>
+        . Alongside my studies, I am actively engaged as a research assistant in
+        the
+        <a
+          className="text-blue-500"
+          href="https://ciol-sust.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Computational Intelligence and Operations Lab - CIOL.
+        </a>
+        
+
+        I am eagerly seeking opportunities to embark on a Ph.D. journey, with a
+        keen interest in the intersection of Optimization and Machine Learning.
+        My primary goal is to leverage the synergy between these domains to
+        drive groundbreaking advancements in business organizations.
+        
+
+        As part of my undergraduate thesis, under the guidance of
+        <a
+          className="text-blue-500"
+          href="https://www.sust.edu/d/ipe/faculty-profile-detail/310"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dr Abul Mukid Mohammad Mukaddes
+        </a>
+        , I am focusing on developing optimal schedules for preparatory leave
+        using prescriptive analytics.
+      </p>
+    </div>
+  );
 };
 
 export default HomeDescreption;
