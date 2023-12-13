c2025c5412652c0142d18c88cb8c80a4928c0382
Shahriar Ahmed <dipto2041@gmail.com>
2023-12-13 22:30:43 +0600

    hero section done

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,13 +1,95 @@
 import React from "react";
 import sohan from "../assets/images/sohanImage.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLocationPin,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faHackerrank,
+  faKaggle,
+  faLinkedin,
+  faResearchgate,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Hero = () => {
   return (
     <div className="hero min-h-screen p-20">
-      <div className="hero-content lg:flex-row">
-        <img src={sohan} alt="sohan" className="max-w-sm rounded-3xl shadow-2xl" />
+      <div className="hero-content flex-col justify-between lg:flex-row">
+        <img
+          src={sohan}
+          alt="sohan"
+          className="max-w-sm rounded-3xl shadow-2xl"
+        />
         <div>
           <h1 className="text-2xl font-bold">MD SHAFIKUL ISLAM</h1>
+          <p className="text-xl">
+            BSC in INDUSTRIAL AND PRODUCTION ENGINEERING
+          </p>
+          <br />
+          <p>SHAHJALAL UNIVERSITY OF SCIENCE AND TECHNOLOGY</p>
+          <br />
+          <p>
+            <small>
+              <FontAwesomeIcon icon={faPhone} /> 01760329288
+            </small>
+          </p>
+          <p>
+            <small>
+              <FontAwesomeIcon icon={faEnvelope} /> Shafikul37@student.sust.edu
+            </small>
+          </p>
+          <p>
+            <small>
+              <FontAwesomeIcon icon={faLocationPin} /> DHAKA, BANGLADESH
+            </small>
+          </p>
+          <br />
+          <br />
+          <div>
+            <a
+              className="m-0"
+              href="https://www.linkedin.com/in/md-shafikul-islam-sohan/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a
+              className="m-3"
+              href="https://github.com/shafikul-islam"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a
+              className="m-3"
+              href="https://www.researchgate.net/profile/Md-Islam-2168"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faResearchgate} />
+            </a>
+            <a
+              className="m-3"
+              href="https://www.kaggle.com/mdshafikulislamsohan "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faKaggle} />
+            </a>
+            <a
+              className="m-3"
+              href="https://www.hackerrank.com/sohan11807044"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faHackerrank} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/pages/HomeDescreption.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeDescreption = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomeDescreption;
