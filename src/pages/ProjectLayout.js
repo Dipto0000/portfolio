561c6d26061b13030fe67509c9c67a99953f355a
Shahriar Ahmed <dipto2041@gmail.com>
2024-01-14 12:27:15 +0600

    Done till supply chain

--- src/pages/ProjectLayout.js
+++ src/pages/ProjectLayout.js
@@ -6,7 +6,9 @@ const ProjectLayout = () => {
   return (
     <div>
       <h1 className="text-2xl m-5">
-        I am currently immersed in several research projects. You can navigate through different kinds of projects. My projects are listed below according to their types. You can click on the links to see the projects.
+        I am currently immersed in several research projects. You can navigate
+        through different kinds of projects. My projects are listed below
+        according to their types. Please click on the links to see the projects.
       </h1>
 
       <ProjectNestedNavbar></ProjectNestedNavbar>
